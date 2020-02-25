b55a5baf3c481dc71b531159d63fd382f88199f0
Devraj Mehta <devm33@gmail.com>
2020-02-25 11:47:16 -0500

    add firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCpFXQQWizCryf6Qt9aSE4zxd0x92yLYJM",
+    authDomain: "devm33-temp.firebaseapp.com",
+    databaseURL: "https://devm33-temp.firebaseio.com",
+    projectId: "devm33-temp",
+    storageBucket: "devm33-temp.appspot.com",
+    messagingSenderId: "167041695122",
+    appId: "1:167041695122:web:63c50bff7ff1d7c7f7554b",
+    measurementId: "G-Y7K3ZC6VV1",
+  }
 };
