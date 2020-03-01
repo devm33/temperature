71f256e34cb13692de4e6af406f709fe5e9e5afe
Devraj Mehta <devm33@gmail.com>
2020-03-01 00:03:08 -0500

    add rolling average

--- firebase/functions/src/index.spec.ts
+++ firebase/functions/src/index.spec.ts
@@ -1,3 +1,4 @@
+// tslint:disable-next-line
 import "jasmine";
 
 import * as setupTests from 'firebase-functions-test';
@@ -11,29 +12,47 @@ const test = setupTests({
 import * as admin from 'firebase-admin';
 
 import { addTemperature } from './index';
-import { WrappedFunction } from 'firebase-functions-test/lib/main';
 
-
-describe('Cloud Functions', () => {
+describe('addTemperature', () => {
+  afterAll(() => {
+    test.cleanup();
+  })
 
   afterEach(async () => {
-    test.cleanup();
     await admin.database().ref().remove();
   })
 
-  describe('addTemperature', () => {
-    let wrappedAddTemperature: WrappedFunction;
+  it('writes the latest value to latest', async () => {
+    const recording = { F: 98.7, created: 123456 };
+    const snap = test.database.makeDataSnapshot(recording, '/recordings/1')
 
-    beforeAll(() => {
-      wrappedAddTemperature = test.wrap(addTemperature);
-    })
-    it('writes the latest value to latest', async () => {
-      const recording = { F: 98.7, created: 123456 };
-      const snap = test.database.makeDataSnapshot(recording, '/recordings/1')
-      await wrappedAddTemperature(snap);
-      const latest = await admin.database().ref('/rooms/first/latest')
-        .once('value');
-      await expect(latest.val()).toEqual(recording);
-    });
+    await test.wrap(addTemperature)(snap);
+
+    const latest = await admin.database().ref('/rooms/first/latest')
+      .once('value');
+    await expect(latest.val()).toEqual(recording);
+  });
+
+  it('writes the latest value to seed the rolling average', async () => {
+    const recording = { F: 98.7, created: 123456 };
+    const snap = test.database.makeDataSnapshot(recording, '/recordings/1')
+
+    await test.wrap(addTemperature)(snap);
+
+    const rolling = await admin.database().ref('/rooms/first/rolling')
+      .once('value');
+    await expect(rolling.val()).toEqual(recording.F);
+  });
+
+  it('updates an existing rolling average with the latest value', async () => {
+    const recording = { F: 100, created: 123456 };
+    const snap = test.database.makeDataSnapshot(recording, '/recordings/1')
+    await admin.database().ref('/rooms/first/rolling').set(50);
+
+    await test.wrap(addTemperature)(snap);
+
+    const newRolling = await admin.database().ref('/rooms/first/rolling')
+      .once('value');
+    await expect(newRolling.val()).toEqual(55);
   });
 });
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -7,11 +7,30 @@ admin.initializeApp();
 export const addTemperature = functions.database.ref('/recordings/{recording}')
   .onCreate(handleNewTemperature);
 
+interface Recording {
+  F: number;
+  created: number;
+}
+
 async function handleNewTemperature(
   snapshot: functions.database.DataSnapshot,
   context: functions.EventContext) {
-  const room = admin.database().ref('/rooms/first');
-  await room.child('latest').set(snapshot.val());
-  // TODO add ten minute average
+  const roomRef = admin.database().ref('/rooms/first');
+  const recording = <Recording>snapshot.val();
+
+  // Update the latest value.
+  await roomRef.child('latest').set(recording);
+
+  // Update the last ten rolling average.
+  const room = await roomRef.once('value');
+  if (!room.hasChild('rolling')) {
+    await roomRef.child('rolling').set(recording.F);
+  } else {
+    const rolling = <number>room.child('rolling').val();
+    await roomRef.child('rolling')
+      .set(rolling - (rolling / 10) + (recording.F / 10));
+  }
+
   // TODO group recording data by hour (and then delete).
+
 }
