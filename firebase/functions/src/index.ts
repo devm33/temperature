9525a2ae249c885c10e9f5bcc6967c0505324e99
Devraj Mehta <devm33@gmail.com>
2020-03-01 22:53:54 -0500

    archiving old recordings into hourly averages in progress

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,27 +1,47 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as moment from 'moment';
 
 admin.initializeApp();
 
-// Listens for new temperature recordings and updates fields accordingly.
-export const addTemperature = functions.database.ref('/recordings/{recording}')
-  .onCreate(handleNewTemperature);
+// Temperature recordings list: /recordings
+interface RecordingList {
+  [id: string]: Recording;
+}
 
+// Temperature recording: /recordings/{recording}
 interface Recording {
   F: number;
   created: number;
 }
 
+// Hourly average values /hourly/first/{milliseconds}
+interface HourlyAverage {
+  average: number;
+  count: number;
+}
+
+// One hour from the server timestamp of the current recording.
+const ONE_HOUR_AGO = moment(admin.database.ServerValue.TIMESTAMP)
+  .subtract(1, 'hour');
+
+// Listens for new temperature recordings and updates database accordingly.
+export const addTemperature = functions.database.ref('/recordings/{recording}')
+  .onCreate(handleNewTemperature);
+
 async function handleNewTemperature(
   snapshot: functions.database.DataSnapshot,
   context: functions.EventContext) {
   const roomRef = admin.database().ref('/rooms/first');
-  const recording = <Recording>snapshot.val();
+  const latest = <Recording>snapshot.val();
+  await roomRef.child('latest').set(latest);
+  await updateRollingAverage(latest);
+  await archiveOldRecordings();
+}
 
-  // Update the latest value.
-  await roomRef.child('latest').set(recording);
-
-  // Update the last ten rolling average.
+// Updates a rolling average to estimate the average of the last ten values.
+async function updateRollingAverage(recording: Recording) {
+  const roomRef = admin.database().ref('/rooms/first');
   const rolling = await roomRef.child('rolling').once('value');
   if (!rolling.exists()) {
     await roomRef.child('rolling').set(recording.F);
@@ -30,7 +50,35 @@ async function handleNewTemperature(
     await roomRef.child('rolling')
       .set(rollingVal - (rollingVal / 10) + (recording.F / 10));
   }
+}
 
-  // TODO group recording data by hour (and then delete).
+// Finds any recordings older than an hour, adds them to hourly averages, and
+// then deletes them.
+async function archiveOldRecordings() {
+  const recordingsRef = admin.database().ref('/recordings');
+  const recordings = <RecordingList>(await recordingsRef.once('value')).val();
+  const toBeArchived = Object.values(recordings).filter(isOldRecording);
+  for (const recording of toBeArchived) {
+    await addToHourlyAverage(recording);
+    await deleteRecording(recording);
+  }
+}
 
+// Returns true if the recording is older than an hour ago.
+function isOldRecording(recording: Recording): boolean {
+  return moment(recording.created).isAfter(ONE_HOUR_AGO);
+}
+
+// Adds a recording to its hourly average.
+async function addToHourlyAverage(recording: Recording) {
+  const created = moment(recording.created);
+  const hourPath = `/hourly/first/${created.startOf('hour').valueOf()}`;
+  const averageRef = admin.database().ref(hourPath);
+  const average = <HourlyAverage>(await averageRef.once('value')).val();
+  // TODO add to average
+}
+
+// Removes a recording from the database.
+async function deleteRecording(recording: Recording) {
+  // TODO
 }
